63d0fd9775365e6007912d6142be2af4707bbfd3
Safia Abdale <safia.abdale@gmail.com>
2021-12-09 04:13:02 +0000

    Enabled submitting new sites to the database, and linking tiny URLs to original website

--- express_server.js
+++ express_server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
 app.set("view engine", "ejs");
-
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,8 +12,7 @@ const urlDatabase = {
 
 function generateRandomString() {
   return Math.random().toString(36).slice(2, 8);
-};
-
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -41,8 +37,9 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  // res.send("Ok"); // Respond with 'Ok' (we will replace this)
-  res.send(generateRandomString())
+  const randomStr = generateRandomString();
+  urlDatabase[randomStr] = req.body.longURL; // Pushes new tiny URL to urlDatabase
+  res.redirect(`/urls/${randomStr}`); // Redirects to newly made hyperlinked page
 });
 
 app.get("/urls/new", (req, res) => {
@@ -52,4 +49,9 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
