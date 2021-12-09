6eb2a31422ff5afc5d9f8ad97a70b6e1b3205ae3
Safia Abdale <safia.abdale@gmail.com>
2021-12-09 03:18:56 +0000

    Updated form to take a URL and return a random 6-character string

--- express_server.js
+++ express_server.js
@@ -4,10 +4,19 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  return Math.random().toString(36).slice(2, 8);
+};
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -28,6 +37,12 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  // res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  res.send(generateRandomString())
 });
 
 app.get("/urls/new", (req, res) => {
