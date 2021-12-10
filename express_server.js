03a04413509c3e5db4f0d47258ce49f1ee5349e6
Safia Abdale <safia.abdale@gmail.com>
2021-12-10 04:04:57 +0000

    Added functional login and logout buttons in header

--- express_server.js
+++ express_server.js
@@ -3,16 +3,29 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const cookieParser = require('cookie-parser');
 
-const urlDatabase = {
+// Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+// Data
+const urlDatabase = { // placeholder URLs
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-function generateRandomString() {
+// const users = [ // placeholder login
+//   {username: 'safia', password: 'passw0rd'}
+// ];
+
+function generateRandomString() { 
   return Math.random().toString(36).slice(2, 8);
 }
+
+
+
+// ROUTES
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -30,40 +43,73 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// Homepage ✅
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
+// Create new URL ✅
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   const randomStr = generateRandomString();
   urlDatabase[randomStr] = req.body.longURL; // Pushes new tiny URL to urlDatabase
-  res.redirect(`/urls/${randomStr}`); // Redirects to tinyURL page
+  res.redirect(`/urls/${randomStr}`); // Redirects to main /urls page
 });
 
+// Lists short URL on homepage ✅
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// Assign shortURL to longURL ✅
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+// Delete URL ✅
 app.post("/urls/:shortURL/delete", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   delete urlDatabase[req.params.shortURL], req.params.shortURL; // Deletes URL entry
   res.redirect("/urls"); // Redirects to main urls_index page
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
-});
-
+// Hyperlinks short URL to long URL ✅ only from new URL pg
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+// Edit URL ✅
 app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.newLongURL;
   // res.redirect(`/urls/${req.params.shortURL}`);
   res.redirect(`/urls/${req.params.shortURL}`);
-})
+});
+
+// login ✅
+app.get("/login", (req, res) => {
+  res.redirect("/urls")
+});
+
+// Collects cookie on login ✅
+app.post("/login", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+
+  res.cookie("username", req.body.username); // value = name of form from _header.ejs
+
+  res.redirect("/urls"); // Redirects to main /urls page
+});
+
+// logout ✅
+app.get("/logout", (req, res) => {
+  res.redirect("/urls")
+});
+
+// Removes cookie on logout ✅
+app.post("/logout", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.clearCookie("username"); // Removes the cookie + username
+  res.redirect("/urls"); // Redirects to main /urls page
+});
